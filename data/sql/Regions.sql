772a2cbb9474f503151afb06080dc6ffede1b063
juan carlos mordan <juan.mordan@gmail.com>
2019-01-08 08:21:46 -0400

    test

--- data/sql/Regions.sql
+++ data/sql/Regions.sql
@@ -1,3 +1,5 @@
+SET IDENTITY_INSERT  [dbo].[Regions] ON;  
+
 INSERT [dbo].[Regions] ([RegionId], [Name]) VALUES (N'01', N'Cibao Norte')
 INSERT [dbo].[Regions] ([RegionId], [Name]) VALUES (N'02', N'Cibao Sur')
 INSERT [dbo].[Regions] ([RegionId], [Name]) VALUES (N'03', N'Cibao Nordeste')
@@ -8,3 +10,5 @@ INSERT [dbo].[Regions] ([RegionId], [Name]) VALUES (N'07', N'Valle')
 INSERT [dbo].[Regions] ([RegionId], [Name]) VALUES (N'08', N'Yuma')
 INSERT [dbo].[Regions] ([RegionId], [Name]) VALUES (N'09', N'Higuamo')
 INSERT [dbo].[Regions] ([RegionId], [Name]) VALUES (N'10', N'Ozama')
+
+SET IDENTITY_INSERT  [dbo].[Regions] Off;  
